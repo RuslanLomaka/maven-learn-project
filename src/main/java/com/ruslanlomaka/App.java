fcb5131d017b9311928fe9a7ff6c703082f9f3f9
margulis <wfptny@gmail.com>
2024-12-26 17:39:24 +0100

    MVN LRN

---
+++ src/main/java/com/ruslanlomaka/App.java
@@ -0,0 +1,27 @@
+package com.ruslanlomaka;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class App {
+    public static void main(String[] args) {
+
+        Person me = new Person("Ruslan", "Lomaka");
+
+        Gson myGson = new GsonBuilder().setPrettyPrinting().create();
+
+        String ruslanLomaka = myGson.toJson(me);
+
+        System.out.println(ruslanLomaka);
+    }
+}
+
+class Person {
+    String name;
+    String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
